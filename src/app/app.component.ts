2e451e21cc7d23ade422739c89d5cf2dc67a35aa
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-11-12 06:05:47 +0200

    style: adding the layout of dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list'; 
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  ViewChild,
+} from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatButtonModule],
+  imports: [RouterOutlet,MatButtonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    NgIf,
+    NgClass
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'stations-dashboard';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  isMobile= true;
+  isCollapsed = false;
+
+  public sideNavitems = [
+    { path: 'control-room', title: 'Control room',svg:"control-room.svg" },
+    { path: 'dashboard', title: 'Dashboard',svg:"dashboard-apps.svg" },
+    { path: 'elements', title: 'Elements',svg:"elements.svg" },
+    { path: 'settings', title: 'Settings',svg:"settings.svg" },
+    { path: 'notifications', title: 'Notifications',svg:"notifications.svg" },
+  ];
+
+  constructor(private observer: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if(screenSize.matches){
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+
+  toggleMenu() {
+    if(this.isMobile){
+      this.sidenav.toggle();
+      this.isCollapsed = false; // On mobile, the menu can never be collapsed
+    } else {
+      this.sidenav.open(); // On desktop/tablet, the menu can never be fully closed
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
+
 }
